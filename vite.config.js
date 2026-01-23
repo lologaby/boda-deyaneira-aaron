d44a3001750a1122f8bb5ede221d6df41e2f37ab
lologaby <agbm97@me.com>
2026-01-23 12:11:16 -0400

    ll

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // El punto y la barra './' le dicen: "busca en la carpeta actual"
-  base: './', 
+  base: '/boda-deyaneira-aaron/',
 })
