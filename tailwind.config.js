767e2755def38364c075f333e30f941fad66b64e
lologaby <agbm97@me.com>
2026-01-23 11:40:42 -0400

    Snap

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}", 
+    "src/**/*.{html,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
